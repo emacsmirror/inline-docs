977e4b6bb2e636335c9e72cc9ef5c922296ec3ae
stardiviner <numbchild@gmail.com>
2026-01-15 21:53:09 +0800

    fix void variable `display-line-numbers-mode` issue

--- inline-docs.el
+++ inline-docs.el
@@ -88,7 +88,7 @@ Set `inline-docs-position' to `up' to fix issue that `inline-docs' does not show
 (defun inline-docs--string-display (string apply-face)
   "Show STRING contents below point line until next command with APPLY-FACE."
   ;; note that `display-line-numbers-mode' takes 2 + `line-number-display-width' columns
-  (let* ((total-column-number (if display-line-numbers-mode
+  (let* ((total-column-number (if (and (boundp display-line-numbers-mode) display-line-numbers-mode)
                                   (- (window-body-width) (+ 2 (line-number-display-width)))
                                 (window-body-width)))
          (border-line (make-string total-column-number inline-docs-border-symbol))
